3a7f27b7976283dac8d644e625f8cd2dbc5df8f3
studionannipierifrancesco <studionannipierifrancesco@gmail.com>
2025-12-04 09:16:29 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://LorenzoMoriniDev.github.io',
+  site: 'https://studiofisiolistiklivornopisa.it/',
   base: '/test-svelte-blog',
 });
