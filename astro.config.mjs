2c5bf7d06fd9b1ace31b3a3be16295e9bd220f14
studionannipierifrancesco <studionannipierifrancesco@gmail.com>
2025-12-10 23:23:43 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  base: '/', 
+  base: '/studionannipierifrancesco.github.io', 
   site: 'https://studiofisiolistiklivornopisa.it',
 });
